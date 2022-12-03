a304bf9a4ba52ebc9798e56cadfa58b19d4b9b0c
vmottin <vmottin@alunos.utfpr.edu.br>
2022-12-03 11:49:16 -0300

    versão final
    últimas alterações para prova do 4° bimestre de DW

--- Prova_3bim/recursos/script.js
+++ Prova_3bim/recursos/script.js
@@ -2,9 +2,9 @@ function Login(){
     let user = document.getElementById("login").value
     let senha = document.getElementById("senha").value
     
-    if(user == 'victoria' && senha == 'dw'){
+    if(user == 'rafaelLiberato' && senha == 'dw1_2022'){
         window.location.href = 'main.html';
     } else{
-        alert("Senha incorreta")
+        alert("Dados incorretos. \nLogin: 'rafaelLiberato' \nSenha: 'dw1_2022'")
     }
 }
